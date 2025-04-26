ebbd91bf2a5f826ab1b0e9dd7af2082f60fc39b3
Ahmed-Elimam <aa.emam97@gmail.com>
2025-04-26 22:01:03 +0300

    fetching Movies

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { TmdbService } from '../../services/tmdb.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movies',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.css'
 })
 export class MoviesComponent {
+trendingMovies = <any>[]
+  constructor(private _tmdbService:TmdbService){}
+  
+  ngOnInit(): void {
+     this._tmdbService.getTrendingMovies().subscribe({
+      next: (data) => {
+        this.trendingMovies = data.results;},
+      error: (error) => {
+        console.error('Error fetching movies:', error);
+      },
+      complete: () => {
+        console.log('Movie fetch complete');
+      }})
 
+    }
 }
--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -7,13 +7,22 @@ import { Observable } from 'rxjs';
 })
 export class TmdbService {
   private apiKey: string = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjJlMGM5Yjk3MjEzODY2MTM2ZGQxZTRlNjBkYzU2NSIsIm5iZiI6MTc0NTY3MjQwNC41NTcsInN1YiI6IjY4MGNkOGQ0MTYzNWQyOTE4YzgwZDkxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h6ngYKNEil9E6hump3b1mWlzSmuSdgQWKEFOz6IBOEI";
+  private headers = new HttpHeaders({
+    'Authorization': `Bearer ${this.apiKey}`});
+
   constructor(private _httpClient:HttpClient) { 
   }
   getAllTrending(): Observable<any> {
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.apiKey}`,
-    });
-    return this._httpClient.get('https://api.themoviedb.org/3/trending/all/day?language=en-US',{headers})
+   
+    return this._httpClient.get('https://api.themoviedb.org/3/trending/all/day?language=en-US',{headers:this.headers})
   }
-
+  getTrendingMovies(): Observable<any> {
+    return this._httpClient.get('https://api.themoviedb.org/3/trending/movie/day?language=en-US',{headers:this.headers})
+  }
+  getTrendingTV(): Observable<any> {
+    return this._httpClient.get('https://api.themoviedb.org/3/trending/tv/day?language=en-US',{headers:this.headers})
+  }
+  getTrendingPeople(): Observable<any> {
+    return this._httpClient.get('https://api.themoviedb.org/3/trending/person/day?language=en-US',{headers:this.headers})
+  }
 }
