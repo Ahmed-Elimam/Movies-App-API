571b81aab57ccd40ece82e36b2502574fefd2fb0
Ahmed-Elimam <aa.emam97@gmail.com>
2025-04-27 12:44:22 +0300

    moving API key out of Service

---
+++ environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    apiKey: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjJlMGM5Yjk3MjEzODY2MTM2ZGQxZTRlNjBkYzU2NSIsIm5iZiI6MTc0NTY3MjQwNC41NTcsInN1YiI6IjY4MGNkOGQ0MTYzNWQyOTE4YzgwZDkxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h6ngYKNEil9E6hump3b1mWlzSmuSdgQWKEFOz6IBOEI",
+    production: false   
+}
--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from  '../../../environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TmdbService {
-  private apiKey: string = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjJlMGM5Yjk3MjEzODY2MTM2ZGQxZTRlNjBkYzU2NSIsIm5iZiI6MTc0NTY3MjQwNC41NTcsInN1YiI6IjY4MGNkOGQ0MTYzNWQyOTE4YzgwZDkxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h6ngYKNEil9E6hump3b1mWlzSmuSdgQWKEFOz6IBOEI";
+  private apiKey: string = environment.apiKey;
   private headers = new HttpHeaders({
     'Authorization': `Bearer ${this.apiKey}`});
 
